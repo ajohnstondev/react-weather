b9c11fbb1ba8b3217e5f1c6f24611f72690e4c64
oleg-serf <olegserfcompany@gmail.com>
2019-11-26 14:29:04 +0100

    Add SVG loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { ReactComponent as Logo } from '../../svg/puff.svg';
+
 import './Loader.scss';
 
 class Loader extends Component {
@@ -9,6 +11,7 @@ class Loader extends Component {
       <div className="Loader">
         <p>Looking outside for you...</p>
         <p>One second...</p>
+        <Logo />
       </div>
     )
   }
