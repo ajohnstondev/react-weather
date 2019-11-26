170443154e43b16467b1b76795be175dd3b0e0a8
oleg-serf <olegserfcompany@gmail.com>
2019-11-26 13:24:10 +0100

    2 services to use IPData and OpenWeather API

---
+++ src/services/IPData.js
@@ -0,0 +1,16 @@
+class IPData {
+
+  constructor() {
+    this.URL = 'https://api.ipdata.co/?api-key='
+    this.key = process.env.REACT_APP_IP_DATA_API_KEY;
+  }
+
+  async getData() {
+    const response = await fetch(`${this.URL}${this.key}`);
+    const data = await response.json();
+
+    return data;
+  }
+}
+
+export default IPData;
---
+++ src/services/OpenWeather.js
@@ -0,0 +1,27 @@
+class OpenWeather {
+
+  constructor() {
+    this.URL = 'https://api.openweathermap.org/data/2.5/weather';
+    this.key = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
+    this.data = {};
+  }
+
+  async getData(ipdata) {
+    const url = new URL(this.URL)
+    const params = {
+      lat: ipdata.latitude,
+      lon: ipdata.longitude,
+      units: 'metric',
+      appid: this.key
+    }
+
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+  }
+}
+
+export default OpenWeather;
